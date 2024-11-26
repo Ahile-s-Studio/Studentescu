51734a6cc4bccf963be323a2f523fc5244430884
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-11-26 12:32:28 +0200

    Add prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './Views/**/*.cshtml',
-        './wwwroot/**/*.html'
-    ],
+    content: ['./Views/**/*.cshtml', './wwwroot/**/*.html'],
     theme: {
         extend: {},
     },
     plugins: [],
-}
+};
