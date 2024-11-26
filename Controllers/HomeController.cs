726b03ad2eb99238f3a6e39b3f398f2c42b28a53
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-11-26 12:57:33 +0200

    Test lint-staged

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+
 using Microsoft.AspNetCore.Mvc;
+
 using Studentescu.Models;
 
 namespace Studentescu.Controllers;
--- Program.cs
+++ Program.cs
@@ -32,7 +32,7 @@ else
 }
 
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection( );
 app.UseStaticFiles();
 
 
