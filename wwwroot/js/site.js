50d4cd61eb11ba8211c4eab835233732221de3d7
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-11-27 15:45:28 +0200

    Update docker-compose

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,6 +2,6 @@
 // Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 console.log("TEST");
-var h = document.querySelector("h1");
-if (h) h.innerHTML = "TEST";
+// const h = document.querySelector<HTMLElement>("h1");
+// if (h) h.innerHTML = "TEST";
 // Write your JavaScript code.
