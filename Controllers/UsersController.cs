d039bd6a4324975a216d02e16c84e275417ea177
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-12-15 17:06:09 +0200

    Admin can see specific user without deleting

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Studentescu.Data;
 using Studentescu.Models;
 using Studentescu.ViewModels;
@@ -20,6 +21,7 @@ public class UsersController : BaseController
     {
     }
 
+
     public IActionResult Index()
     {
         var users = _dbContext.Users.ToList();
@@ -30,6 +32,28 @@ public class UsersController : BaseController
 
         return View(viewModel);
     }
+
+    public async Task<ActionResult> Show(string id)
+    {
+        var user = await _dbContext.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var userCurent = await _userManager.GetUserAsync(User);
+
+        UsersShowViewModel viewModel = new()
+        {
+            User = user,
+            Roles = roles,
+            UserCurent = userCurent!
+        };
+
+        return View(viewModel);
+    }
+
 
     public async Task<IActionResult> Edit(string? id)
     {
@@ -65,6 +89,99 @@ public class UsersController : BaseController
             Console.WriteLine();
             return NotFound();
         }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Edit(string id,
+        ApplicationUser newData, [FromForm] string newRole)
+    {
+        var user = _dbContext.Users.Find(id);
+
+        // user.AllRoles = GetAllRoles();
+
+
+        if (!ModelState.IsValid || user == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        user.UserName = newData.UserName;
+        user.Email = newData.Email;
+        user.FirstName = newData.FirstName;
+        user.LastName = newData.LastName;
+        user.PhoneNumber = newData.PhoneNumber;
+
+
+        var roles = _dbContext.Roles.ToList();
+
+        foreach (var role in roles)
+        {
+            if (role.Name != null)
+            {
+                await _userManager.RemoveFromRoleAsync(user,
+                    role.Name);
+            }
+        }
+
+        var roleName = await _roleManager.FindByIdAsync(newRole);
+        if (roleName == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        await _userManager.AddToRoleAsync(user,
+            roleName.ToString());
+
+        _dbContext.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+
+
+    [HttpPost]
+    public IActionResult Delete(string id)
+    {
+        var user = _dbContext.Users
+            .Include("Followers")
+            .Include("Followings")
+            .Include("Posts")
+            .Include("Comments")
+            .Include("RequestsSent")
+            .Include("RequestsReceived")
+            .Include("GroupMemberships")
+            .First(u => u.Id == id);
+
+        // // Delete user comments
+        // if (user.Comments.Count > 0)
+        // {
+        //     foreach (var comment in user.Comments)
+        //     {
+        //         db.Comments.Remove(comment);
+        //     }
+        // }
+        //
+        // // Delete user bookmarks
+        // if (user.Bookmarks.Count > 0)
+        // {
+        //     foreach (var bookmark in user.Bookmarks)
+        //     {
+        //         db.Bookmarks.Remove(bookmark);
+        //     }
+        // }
+        //
+        // // Delete user articles
+        // if (user.Articles.Count > 0)
+        // {
+        //     foreach (var article in user.Articles)
+        //     {
+        //         db.Articles.Remove(article);
+        //     }
+        // }
+
+        _dbContext.Users.Remove(user);
+
+        _dbContext.SaveChanges();
+        return RedirectToAction("Index");
     }
 
     [NonAction]
