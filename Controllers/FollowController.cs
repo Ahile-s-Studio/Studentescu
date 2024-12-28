3111334296512b4a8331a39e644ce3cfddb90c84
Darius-Ștefan Țacu <88737250+stefanFortza@users.noreply.github.com>
2024-12-28 14:51:49 +0200

    Implement follow, follow request, and accept/reject requests

--- Controllers/FollowController.cs
+++ Controllers/FollowController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Studentescu.Data;
 using Studentescu.Models;
+using Studentescu.Services;
 
 namespace Studentescu.Controllers;
 
@@ -10,11 +12,14 @@ namespace Studentescu.Controllers;
 [Authorize(Roles = "Admin,User")]
 public class FollowController : BaseController
 {
+    private readonly FollowService _followService;
+
     public FollowController(ILogger<HomeController> logger,
         ApplicationDbContext context, UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager) : base(logger, context,
+        RoleManager<IdentityRole> roleManager, FollowService followService) : base(logger, context,
         userManager, roleManager)
     {
+        _followService = followService;
     }
 
 
@@ -22,10 +27,10 @@ public class FollowController : BaseController
     public async Task<IActionResult> GetFollowing()
     {
         var user = await _userManager.GetUserAsync(User);
-        if (user == null)
-        {
-            return Unauthorized();
-        }
+        // if (user == null)
+        // {
+        //     return Unauthorized();
+        // }
 
         var followingUsersIds = _dbContext.Follows.Where(f => f.FollowerId == user.Id)
             .Select(f => f.FolloweeId).ToList();
@@ -33,6 +38,7 @@ public class FollowController : BaseController
         var followingUsers = _dbContext.Users
             .Where(u => followingUsersIds.Contains(u.Id))
             .ToList();
+
 
         return Ok(followingUsers);
     }
@@ -56,6 +62,67 @@ public class FollowController : BaseController
         return Ok(followerUsers);
     }
 
+    [HttpGet("requests/sent")]
+    public async Task<IActionResult> GetRequestsSent()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var requestedUsersIds = _dbContext.FollowRequests.Where(f => f.RequesterId == user.Id)
+            .Select(f => f.TargetId).ToList();
+
+        var requestedUsers = _dbContext.Users
+            .Where(u => requestedUsersIds.Contains(u.Id))
+            .ToList();
+
+        return Ok(requestedUsers);
+    }
+
+    [HttpGet("requests/received")]
+    public async Task<IActionResult> GetRequestsReceived()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var requestsOfUsersIds = _dbContext.FollowRequests.Where(f => f.TargetId == user.Id)
+            .Select(f => f.TargetId).ToList();
+
+        var requestsOfUsers = _dbContext.Users
+            .Where(u => requestsOfUsersIds.Contains(u.Id))
+            .ToList();
+
+        return Ok(requestsOfUsers);
+    }
+
+    [HttpGet("requests/sent/pending")]
+    public async Task<IActionResult> GetPendingRequestsSent()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var requestedUsers = await _dbContext.FollowRequests
+            .Where(fr => fr.RequesterId == user.Id && fr.Status == FollowRequestStatus.Pending)
+            .Join(
+                _dbContext.Users,
+                fr => fr.TargetId, // Assuming TargetId is the requested user's ID
+                u => u.Id,
+                (fr, u) => u
+            )
+            .ToListAsync();
+
+
+        return Ok(requestedUsers);
+    }
+
     [HttpPost("follow/{userId}")]
     public async Task<IActionResult> FollowUser(string userId)
     {
@@ -71,15 +138,44 @@ public class FollowController : BaseController
             return NotFound();
         }
 
-        var followingIds = _dbContext.Follows.Where(f => f.FollowerId == user.Id).Select(f => f
-            .FolloweeId);
-
-        if (!followingIds.Contains(targetUser.Id))
+        if (targetUser.Id == user.Id)
         {
-            var follow = new Follow { FollowerId = user.Id, FolloweeId = targetUser.Id };
+            return BadRequest(new { error = "You cannot follow yourself." });
+        }
+
+        var follow = await _followService.IsFollowing(user.Id, targetUser.Id);
+
+        if (follow != null)
+        {
+            return Ok(new { message = "User followed successfully" });
+        }
+
+        // Daca e un profil public
+        if (targetUser.Public)
+        {
+            follow = new Follow { FollowerId = user.Id, FolloweeId = targetUser.Id };
             _dbContext.Follows.Add(follow);
             await _dbContext.SaveChangesAsync();
+            return Ok(new { message = "User followed successfully" });
         }
+
+        var followRequest = await _followService.IsRequestSent(user.Id, targetUser.Id);
+
+        // Trimitem follow request
+        if (followRequest != null && followRequest.Status != FollowRequestStatus.Rejected)
+        {
+            return Ok(new { message = "User followed successfully" });
+        }
+
+        followRequest = new FollowRequest
+        {
+            RequesterId = user.Id,
+            TargetId = targetUser.Id,
+            Status = FollowRequestStatus.Pending
+        };
+        _dbContext.FollowRequests.Add(followRequest);
+        await _dbContext.SaveChangesAsync();
+
 
         return Ok(new { message = "User followed successfully" });
     }
@@ -105,6 +201,15 @@ public class FollowController : BaseController
         if (follow != null)
         {
             _dbContext.Follows.Remove(follow);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        var followRequest = _dbContext.FollowRequests.FirstOrDefault(f =>
+            f.RequesterId == user.Id && f.TargetId == targetUser.Id);
+
+        if (followRequest != null)
+        {
+            _dbContext.FollowRequests.Remove(followRequest);
             await _dbContext.SaveChangesAsync();
         }
 
